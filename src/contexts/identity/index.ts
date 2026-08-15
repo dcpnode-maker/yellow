d10ca7556be0404305b910dd5d28281c81e27c17
Astha <astha@localhost>
2026-08-15 17:35:09 +0000

    [codex] implement tenant-safe org hierarchy queries

--- src/contexts/identity/index.ts
+++ src/contexts/identity/index.ts
@@ -1,6 +1,8 @@
 export { hashLocalPassword, isLocalAuthRecord, verifyLocalPassword } from "./password";
 export type { LocalAuthRecord } from "./password";
 export { BearerTenantResolver } from "./resolver";
+export { OrgHierarchy } from "./org-hierarchy";
+export type { OrgHierarchyNode, OrgNodeKind } from "./org-hierarchy";
 export { Hs256TokenSigner, isValidScope, tokenPolicy } from "./token";
 export type {
   AccessTokenClaims,
